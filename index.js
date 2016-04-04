2b9c7e96c3eef7b37117769085335ddf5d889615
gincwang <gincwang@gmail.com>
2016-04-04 10:59:05 -0700

    fix: pass in socket reference vs io to commonQueries so msg isnt broadcast to everyone

--- index.js
+++ index.js
@@ -67,79 +67,80 @@ io.on('connection', function(socket){
     socket.on('received recent matches', function(matches){
         console.log('found matches: ' + matches);
     });
-});
 
+    //query database if summoner ID already exists
+    //If exists, fire off request to search recent matches
+    //If doesn't exist, fire off request to search for summoner ID based on name, stores it to database, then request recent matches
+    function querySummoner(name, client, done){
 
-//query database if summoner ID already exists
-//If exists, fire off request to search recent matches
-//If doesn't exist, fire off request to search for summoner ID based on name, stores it to database, then request recent matches
-function querySummoner(name, client, done){
-
-    var queryString = "SELECT summoner_id FROM summoner WHERE  base_name='" + name + "'";
-    client.query(queryString, function(err, result){
-        if(err){
-            console.log('error querying from querySummoner');
-            done();
-        }
-        else {
-            if(result.rowCount === 0){
-                //summoner not in database, need to fetch api
-                console.log('summoner not found in DB');
-                riotSeeder.getSummonerID(name)
-                    .then(function(id){
-                        console.log('rp promise: ' + id);
-                        if(id !== -1){
-                            //request API to get recent games associated with the summonerID
-                            querySummonerMatches(id);
-                        }
-                    })
-                    .catch(function(err){
-                        console.log(err)
-                        io.emit('summoner not found', err);
-                    });
+        var queryString = "SELECT summoner_id FROM summoner WHERE  base_name='" + name + "'";
+        client.query(queryString, function(err, result){
+            if(err){
+                console.log('error querying from querySummoner');
+                done();
             }
             else {
-                var summonerID = result.rows[0].summoner_id;
-                //request API to get recent games associated with the summonerID
-                querySummonerMatches(summonerID);
+                if(result.rowCount === 0){
+                    //summoner not in database, need to fetch api
+                    console.log('summoner not found in DB');
+                    riotSeeder.getSummonerID(name)
+                        .then(function(id){
+                            console.log('rp promise: ' + id);
+                            if(id !== -1){
+                                //request API to get recent games associated with the summonerID
+                                querySummonerMatches(id);
+                            }
+                        })
+                        .catch(function(err){
+                            console.log(err)
+                            io.emit('summoner not found', err);
+                        });
+                }
+                else {
+                    var summonerID = result.rows[0].summoner_id;
+                    //request API to get recent games associated with the summonerID
+                    querySummonerMatches(summonerID);
+                }
+                //close db connection
+                done();
             }
-            //close db connection
-            done();
-        }
-    });
-}
-
-//Requests summoner's recent matches,
-//If successful, query summoner's stat summary
-function querySummonerMatches(summonerID){
-    console.log('found summoner: ' + summonerID);
-    riotSeeder.getRecentMatches(summonerID)
-        .then(function(id){
-            console.log(id);
-            querySummonerSummary(id);
-        })
-        .catch(function(err){
-            console.log(err);
         });
-}
+    }
 
-//Requests summoner's stat summary
-//If successful, send relevant data back to client
-function querySummonerSummary(summonerID){
-    console.log('found matches');
-    riotSeeder.getSummary(summonerID)
-        .then(function(id){
-            returnStats(id);
-        })
-        .catch(function(err){
-            console.log(err);
-        });
-}
+    //Requests summoner's recent matches,
+    //If successful, query summoner's stat summary
+    function querySummonerMatches(summonerID){
+        console.log('found summoner: ' + summonerID);
+        riotSeeder.getRecentMatches(summonerID)
+            .then(function(id){
+                console.log(id);
+                querySummonerSummary(id);
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+    }
 
-//
-function returnStats(id){
-    queries.getSummonerStats(id, io);
-}
+    //Requests summoner's stat summary
+    //If successful, send relevant data back to client
+    function querySummonerSummary(summonerID){
+        console.log('found matches');
+        riotSeeder.getSummary(summonerID)
+            .then(function(id){
+                returnStats(id);
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+    }
+
+    //
+    function returnStats(id){
+        queries.getSummonerStats(id, socket);
+    }
+
+
+});
 
 http.listen(port, function(){
     console.log('Listening on port...' + port);
