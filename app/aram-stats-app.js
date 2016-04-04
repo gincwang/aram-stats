ec08046915ab2b02ca4ceb5430b7b936553dbbb4
gincwang <gincwang@gmail.com>
2016-04-03 23:39:13 -0700

    fix: clean up ng-app

--- app/aram-stats-app.js
+++ app/aram-stats-app.js
@@ -1,7 +1,9 @@
 
-angular.module('AramStats', [
-    'ui.router'
-])
+angular.module('aramStats', [
+        'ui.router',
+        'aramStats.components.nav',
+        'aramStats.components.summoner'
+    ])
     .config(function($stateProvider, $urlRouterProvider){
         $stateProvider
             .state('aramStats', {
@@ -10,32 +12,4 @@ angular.module('AramStats', [
             });
 
         $urlRouterProvider.otherwise('/');
-    })
-    .controller('MainCtrl', function($scope, $http){
-        $scope.name = '';
-        $scope.region = 'NA';
-        $scope.invalidName = false;
-        $scope.summary = {};
-
-        var socket = io();
-
-        socket.on('found recent matches', function(result){
-            console.log(result);
-        });
-
-        socket.on('return summary stat', function(stat){
-            console.log('receive summary');
-            console.log(stat);
-            $scope.invalidName = false;
-            $scope.summary = stat;
-        });
-
-        socket.on('summoner not found', function(){
-            console.log('summoner not found');
-            $scope.invalidName = true;
-        });
-
-        $scope.getStats = function(name){
-            socket.emit('get summoner', name);
-        }
     });
